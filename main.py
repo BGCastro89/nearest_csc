1ce227d0369528b9744ade20a8ab496ca271d05e
Brian G Castro <prismaticq@gmail.com>
2020-06-26 09:22:48 -0700

    get images from HTTPS

--- main.py
+++ main.py
@@ -84,8 +84,8 @@ def nearest_csc(request):
         if curr_closest_km < MAX_DIST_KM:
             closest_site['status_msg'] = "SUCCESS"
             closest_site['dist_km'] = curr_closest_km
-            closest_site['full_img'] = "http://www.cleardarksky.com/c/"+closest_site['id']+"csk.gif"
-            closest_site['mini_img'] = "http://www.cleardarksky.com/c/"+closest_site['id']+"cs0.gif"
+            closest_site['full_img'] = "https://www.cleardarksky.com/c/"+closest_site['id']+"csk.gif"
+            closest_site['mini_img'] = "https://www.cleardarksky.com/c/"+closest_site['id']+"cs0.gif"
         else: 
             closest_site = {'status_msg': "No sites within 100 km. CSC sites are only available in the Continental US, Canada, and Northern Mexico"}
 
